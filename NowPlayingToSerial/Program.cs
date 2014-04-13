1a1a74662ef358ff725a016eb63b9fd8c079df11
Craig Ceremuga <c.ceremuga@gmail.com>
2014-04-13 11:24:20 -0400

    sln / csproj rename / namespace change

--- NowPlayingToSerial/Program.cs
+++ NowPlayingToSerial/Program.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
-namespace VLCToSerial
+namespace NowPlayingToSerial
 {
     class Program
     {
--- NowPlayingToSerial/Properties/AssemblyInfo.cs
+++ NowPlayingToSerial/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VLCToSerial")]
+[assembly: AssemblyTitle("NowPlayingToSerial")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VLCToSerial")]
+[assembly: AssemblyProduct("NowPlayingToSerial")]
 [assembly: AssemblyCopyright("Copyright © 2014 Craig Ceremuga")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

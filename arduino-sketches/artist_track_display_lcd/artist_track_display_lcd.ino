646a95f19473e0aaff767d142528202df8c4d57e
Craig Ceremuga <c.ceremuga@gmail.com>
2014-04-26 11:13:28 -0400

    Support for Adafruit RGB color LCD's

--- arduino-sketches/artist_track_display_lcd/artist_track_display_lcd.ino
+++ arduino-sketches/artist_track_display_lcd/artist_track_display_lcd.ino
@@ -1,4 +1,16 @@
-#include <LiquidCrystal.h>
+//omit the adafruit imports and adafruit LCD library 
+//specific code if just using standard LiquidCrysal.h
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
+//exclude above and include this if not using an 
+//Adafruit RGB LCD
+//#include <LiquidCrystal.h>
+
+// The shield uses the I2C SCL and SDA pins. On classic Arduinos
+// this is Analog 4 and 5 so you can't use those for analogRead() anymore
+// However, you can connect other I2C sensors to the I2C bus and share
+// the I2C bus.
 
 //configuration
 #define BAUD_RATE 9600
@@ -8,9 +20,18 @@
 #define DELAY_BEFORE_SCROLL 1000
 #define MESSAGE_WAIT 200
 #define SCROLL_PAUSE 1000
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
 
+//comment this out and use the below for non-Adafruit displays
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 //initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   //set up the LCD's number of columns and rows: 
@@ -20,6 +41,7 @@ void setup() {
   Serial.begin(BAUD_RATE);
   
   //print something once we're ready to roll just to keep the user informed
+  lcd.setBacklight(BLUE); //comment out this line if not using Adafruit RGB LCD
   lcd.noAutoscroll();
   lcd.print("Serial active.");
   lcd.setCursor(0, 1);
